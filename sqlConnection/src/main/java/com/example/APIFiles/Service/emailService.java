46dec462eb53bea7952bb78634a062729a7e79a6
baaln <baaln@Daddy>
2024-12-02 18:00:47 +0530

    code committed for email service

--- sqlConnection/src/main/java/com/example/APIFiles/Service/emailService.java
+++ sqlConnection/src/main/java/com/example/APIFiles/Service/emailService.java
@@ -2,12 +2,6 @@ package com.example.APIFiles.Service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import com.sendgrid.*;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-
 import jakarta.mail.Authenticator;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
@@ -37,40 +31,6 @@ public class emailService
 	 * System.out.print(" to , subject and body " +to+ " " +subject+ " " +body);
 	 * mailSender.send(message); }
 	 */
-	  @Value("${sendgrid.api.key}")
-	    private String sendGridApiKey;
-
-	    @Value("${sendgrid.email.from}")
-	    private String fromEmail;
-
-	  public void sendEmail(String to, String subject, String body) throws IOException {
-          System.out.print(" entered into sendEmail  method in emailService class %s.\\n\\n ");
-	        Email from = new Email(fromEmail); // Your verified email
-	          System.out.print(" From Mail is " +fromEmail);
-	        Email To = new Email(to);
-	          System.out.print(" From Mail is " +to);
-	        Content emailContent = new Content("text/plain", body);
-	        Mail mail = new Mail(from, subject, To, emailContent);
-
-	        SendGrid sg = new SendGrid("sendGridApiKey");
-	          System.out.print(" From Mail is " +sendGridApiKey);
-	        Request request = new Request();
-	        
-
-	        try {
-	            request.setMethod(Method.POST);
-	            request.setEndpoint("mail/send");
-	            request.setBody(mail.build());
-	            Response response = sg.api(request);
-	            System.out.println(" Response: " + response.getStatusCode());
-	            System.out.println("Response Body: " + response.getBody());
-	            System.out.println("Response Headers: " + response.getHeaders());
-	            System.out.println("Key Sent successfully");
-	        } catch (IOException ex) {
-	            throw ex;
-	        }
-	    }
-	  
 	  
 	  @Value("${spring.mail.username}")
 	    private String username;
